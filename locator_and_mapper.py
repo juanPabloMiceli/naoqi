f53e51adc6c276cd41e6a02985b1acbb3d954fa8
Juan Pablo Miceli <micelijuanpablo@gmail.com>
2022-06-09 16:10:59 -0300

    locator and mapper [WIP]

--- locator_and_mapper.py
+++ locator_and_mapper.py
@@ -1,6 +1,7 @@
+from logging import Logger
 import numpy as np
 import math
-from geometry import pol2cart, distance, direction, transformed_point
+from geometry import angle, angle_between_vectors, pol2cart, distance, direction, transformed_point
 from logger_factory import LoggerFactory
 
 class QrLocation:
@@ -11,10 +12,11 @@ class QrLocation:
 
 class LocatorAndMapper:
 
-    qrs_data = np.array()
+    qrs_data = np.array([])
     map_axis = None
     map_axis_zero = None
     LOGGER = LoggerFactory.get_logger("LOGGER")
+    nao_location = None
 
     def add_information(self, new_qrs_data):
         if len(new_qrs_data) < 2:
@@ -32,10 +34,10 @@ class LocatorAndMapper:
 
             qr2_qr1_distance = distance(point_qr1, point_qr2)
             x_dir = direction(point_qr1, point_qr2)
-            y_dir = np.array([-self.x_dir[1], self.x_dir[0]])
+            y_dir = np.array([-x_dir[1], x_dir[0]])
             self.map_axis = np.array([x_dir, y_dir])
             self.map_axis_zero = point_qr1
-            angle_between_coordinates = np.arctan2(self.map_axis[1], self.map_axis[0])
+            angle_between_coordinates = angle(self.map_axis[0])
             self.LOGGER.info("Dist: {}".format(qr2_qr1_distance))
             
             self.__add_qr_to_map(point_qr1, qr1.id, angle_between_coordinates)
@@ -43,17 +45,54 @@ class LocatorAndMapper:
             
         # Procedimiento general
         known_qrs_indices = self.__get_known_qrs_indices(new_qrs_data)
-        if len(known_qrs_indices) >= 2:
-            nao_location = self.get_nao_location(known_qrs_indices)
-            self.__add_new_qrs(new_qrs_data, nao_location)
-
-        return nao_location
+        if len(known_qrs_indices) == 2:
+            self.nao_location = self.__get_nao_location(known_qrs_indices, new_qrs_data)
+            # self.__add_new_qrs(new_qrs_data, nao_location)
     
+        return self.get_nao_location()
+
+    def get_nao_location(self):
+        return self.nao_location
+
     def origin_position(self):
         return transformed_point(np.zeros(2), self.map_axis_zero, self.angle_between_coordinates)
 
+    def __get_nao_location(self, known_qrs_indices, new_qrs_data):
+        qr1 = new_qrs_data[known_qrs_indices[0]]
+        qr2 = new_qrs_data[known_qrs_indices[1]]
+        qr1_id = qr1.id
+        qr2_id = qr2.id
+
+        self.LOGGER.info("Getting Nao location, chose qrs {} and {}".format(qr1_id, qr2_id))
+
+        for qr_data in self.qrs_data:
+            if qr_data.id == qr1_id:
+                point_qr1_map = qr_data.point
+            if qr_data.id == qr2_id:
+                point_qr2_map = qr_data.point
+
+        point_qr1_torso = pol2cart(qr1.distance, math.radians(-qr1.angle))
+        point_qr2_torso = pol2cart(qr2.distance, math.radians(-qr2.angle))
+
+        qr2_qr1_torso = direction(point_qr1_torso, point_qr2_torso) 
+        qr2_qr1_map = direction(point_qr1_map, point_qr2_map)
+
+        qr2_qr1_angle_torso = angle(qr2_qr1_torso)
+        qr2_qr1_angle_map = angle(qr2_qr1_map)
+
+        qr1_nao_map_length = qr1.distance
+        qr1_nao_map_angle = (-np.radians(-qr1.angle) - qr2_qr1_angle_map + qr2_qr1_angle_torso) * -1
+
+        qr1_nao_map = pol2cart(qr1_nao_map_length, qr1_nao_map_angle)
+
+        nao_location = np.subtract(point_qr1_map, qr1_nao_map) 
+
+        self.LOGGER.info("Nao location: {}".format(nao_location))
+
+        return nao_location
+
     def __add_qr_to_map(self, point, id, angle_between_coordinates):
-        self.qrs_data = np.append(QrLocation(transformed_point(point, self.map_axis_zero, angle_between_coordinates), id))
+        self.qrs_data = np.append(self.qrs_data, QrLocation(transformed_point(point, self.map_axis_zero, angle_between_coordinates), id))
 
     def __get_known_qrs_indices(self, new_qrs_data):
         known_qrs = []
--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-# This test degrees(alpha) how to use the ALLandMarkDetection module.
-# - We first instantiate a proxy to the ALLandMarkDetection module
-#     Note that this module should be loaded on the robot's NAOqi.
-#     The module output its results in ALMemory in a variable
-#     called "LandmarkDetected"
-# - We then read this AlMemory value and check whether we get
-#   interesting things.
-import argparse
 import math
 import sys
 import time
-from os.path import exists
-
+import cv2
 import numpy as np
+from locator_and_mapper import LocatorAndMapper
 from nao_properties import NaoProperties
 import qi
 
@@ -21,36 +13,49 @@ from qr_detector import QrDetector
 from sonar_adapter import SonarAdapter
 from video_controller import VideoController
 
+images = ['images/174cm24degrees2qrs.jpg', 'images/153cm-15degrees2qrs.jpg']
+
 def main(session, ip, port):
 
     # Stop basic awareness so that nao doesn't move his head when not commanded 
-    AwarenessController(session).set(False)
+    # AwarenessController(session).set(False)
 
-    # Look front for finding qrs
-    HeadController(session).look_front()
+    # # Look front for finding qrs
+    # HeadController(session).look_front()
 
-    video_controller = VideoController(ip, port)
+    # video_controller = VideoController(ip, port)
     sonar_adapter = SonarAdapter()
+    locator_and_mapper = LocatorAndMapper()
+    index = input("Insert index: ")
 
-    while True:
-        time.sleep(1)
-        cv2_image = video_controller.get_current_image()
+    while index != -1:
+        cv2_image = cv2.imread(images[index])
+        cv2_image = cv2.cvtColor(cv2_image, cv2.COLOR_RGB2GRAY)
+        # cv2_image = video_controller.get_current_image()
         qrs_data = QrDetector.get_qrs_information(cv2_image)
         sonar_adapter.write_data(qrs_data)
+        locator_and_mapper.add_information(qrs_data)
+        time.sleep(1)
+        index = input("Insert index: ")
 
-        if len(qrs_data) > 1:
-            qr1 = qrs_data[0]
-            qr2 = qrs_data[1] #Cambiar los signos de angle (math.atan2(y,x))
-            print("qr1: {}, qr2: {}".format(qr1, qr2))
-            point_qr1 = np.array([qr1['distance'] * math.cos(math.radians(-qr1['angle'])), qr1['distance'] * math.sin(math.radians(-qr1['angle']))])
-            point_qr2 = np.array([qr2['distance'] * math.cos(math.radians(-qr2['angle'])), qr2['distance'] * math.sin(math.radians(-qr2['angle']))])
-            qr2_qr1_distance = np.linalg.norm(np.subtract(point_qr2, point_qr1))
-            print("Dist ({})_({}) = {}".format(qr1['id'], qr2['id'], qr2_qr1_distance))
+
+        # if len(qrs_data) > 1:
+        #     qr1 = qrs_data[0]
+        #     qr2 = qrs_data[1] #Cambiar los signos de angle (math.atan2(y,x))
+        #     print("qr1: {}, qr2: {}".format(qr1, qr2))
+        #     point_qr1 = np.array([qr1['distance'] * math.cos(math.radians(-qr1['angle'])), qr1['distance'] * math.sin(math.radians(-qr1['angle']))])
+        #     point_qr2 = np.array([qr2['distance'] * math.cos(math.radians(-qr2['angle'])), qr2['distance'] * math.sin(math.radians(-qr2['angle']))])
+        #     qr2_qr1_distance = np.linalg.norm(np.subtract(point_qr2, point_qr1))
+        #     print("Dist ({})_({}) = {}".format(qr1['id'], qr2['id'], qr2_qr1_distance))
 
 
 if __name__ == "__main__":
     
-    IP, PORT = NaoProperties.get_connection_properties()
+    IP, PORT = NaoProperties().get_connection_properties()
+
+    #Borrar
+    main(None, None, None)
+    exit(0)
 
     # Init session
     session = qi.Session()
