b62aea3dbbd2b6326f699d55478cb1b9651f19de
Juan Pablo Miceli <micelijuanpablo@gmail.com>
2022-07-05 16:20:01 -0300

    Agrego mas imagenes

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from head_controller import HeadController
 from qr_detector import QrDetector
 from sonar_adapter import SonarAdapter
 from video_controller import VideoController
+from photo_saver import PhotoSaver
 
 images = ['images/174cm24degrees2qrs.jpg', 'images/153cm-15degrees2qrs.jpg']
 
@@ -22,24 +23,30 @@ def main(session, ip, port):
     AwarenessController(session).set(False)
 
     # # Look front for finding qrs
-    HeadController(session).look_front()
+    head_controller = HeadController(session)
+    head_controller.look_at(0, 0)
+    
 
     video_controller = VideoController(ip, port)
     sonar_adapter = SonarAdapter()
     map_display_adapter = MapDisplayAdapter()
     locator_and_mapper = LocatorAndMapper()
-    index = input("Insert index: ")
+    photo_saver = PhotoSaver("images/head_angle_test/QRCenterVsImageCenter.jpg")
+
+    index = raw_input("Insert index: ")
 
     while index != -1:
         # cv2_image = cv2.imread(images[index])
         # cv2_image = cv2.cvtColor(cv2_image, cv2.COLOR_RGB2GRAY)
-        cv2_image = video_controller.get_current_image()
-        qrs_data = QrDetector.get_qrs_information(cv2_image)
+        rgb_image = video_controller.get_current_rgb_image()
+        gray_image = video_controller.get_current_gray_image()
+        qrs_data = QrDetector.get_qrs_information(gray_image)
+        QrDetector.find_qrs_and_show(rgb_image, photo_saver.get_save_path(), False)
+        locator_and_mapper.add_information(qrs_data)
         sonar_adapter.write_data(qrs_data)
         map_display_adapter.write_data(locator_and_mapper.get_nao_location(), locator_and_mapper.qrs_data)
-        locator_and_mapper.add_information(qrs_data)
         time.sleep(1)
-        index = input("Insert index: ")
+        index = raw_input("Insert index: ")
 
 
 if __name__ == "__main__":
