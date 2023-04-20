bb181b94e2bdc3b91291466da2158caf33e79b46
Juan Pablo Miceli <micelijuanpablo@gmail.com>
2023-04-20 16:04:56 -0300

    User in Docker is now the same as the one running the process

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run --mount type=bind,source=$PWD,target=/app --rm -it naoqi
+sudo docker run --mount type=bind,source=$PWD,target=/app --rm -it -u $(id -u):$(id -g) naoqi
