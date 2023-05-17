601c80fd0cbc3474e5ab90c4f7e9e79d8103b5c4
Tomas Sujovolsky <tomas.sujovolsky@muttdata.ai>
2023-05-17 17:38:35 -0300

    Added a basic command controller

--- gpt_integration_test/command_controller.py
+++ gpt_integration_test/command_controller.py
@@ -11,17 +11,24 @@ import qi
 
 LOOK_TIMEOUT = 20 # seconds
 
-class GreetController(ALModule):
+postures_vocabularies = {
+    "Standing": ["parar", "parado", "pararse", "parate", "levantate", "de-pie"],
+    "Sitting": ["sentar", "sentado", "sentarse", "sentate"],
+    "LyingBelly": ["acostar", "acostado", "acostarse", "acostate"],
+}
+
+class CommandController(ALModule):
     def __init__(self, ip, port, session):
-        self.name = "greetModule"
+        self.name = "commandController"
         ALModule.__init__(self, self.name)
-        self.LOGGER = LoggerFactory.get_logger("GreetController")
+        self.LOGGER = LoggerFactory.get_logger("CommandController")
         self.awareness = session.service("ALBasicAwareness")
         self.people_perception = session.service("ALPeoplePerception")
         self.face_detection = session.service("ALFaceDetection")
         # self.animation_player = session.service("ALAnimationPlayer")
         # self.wave_detection = session.service("ALWavingDetection")
         self.text_to_speech = session.service("ALTextToSpeech")
+        self.speech_recognition = session.service("ALSpeechRecognition")
         self.memory = session.service("ALMemory")
 
         self.waved_person_id = None
@@ -44,47 +51,78 @@ class GreetController(ALModule):
         self.LOGGER.info("Using full body rotation for engagement")
         self.awareness.setTrackingMode("BodyRotation")
 
+        # set the speech recognition variables
+        self.speech_recognition.setLanguage("spanish")
+        self.speech_recognition.setAudioExpression(True)
+        self.speech_recognition.setVisualExpression(True)
+        known_vocbulary = []
+        for _, posture_vocabulary in postures_vocabularies.items():
+            for word in posture_vocabulary:
+                known_vocbulary.append(word)
+        self.speech_recognition.setVocabulary(known_vocbulary, False)
+
+
         # subscribe to wave detection
         self.LOGGER.info("Subscribing to wave detection events")
         self.memory.subscribeToEvent(
-            "PeoplePerception/JustArrived",
+            "GazeAnalysis/PersonStartsLookingAtRobot",
             self.getName(),
-            "greet_detected"
+            "looked_at"
         )
         while 1:
             continue
 
-    def greet_detected(
+    def looked_at(
             self,
             event_name,
             person_id,
             subscriber_identifier
         ):
-        self.LOGGER.info("Has been waved by {}.".format(person_id))
+        self.LOGGER.info("Has been looked by {}.".format(person_id))
         # save into memory the id of the person waving at the NAO
         self.waved_person_id = person_id
 
-        # only for testing
         self.wave()
 
+        # subscribe to the sound detection module for 3 seconds
+        self.memory.subscribeToEvent(
+            "SpeechRecognition/WordRecognized",
+            self.getName(),
+            "talked_at"
+        )
+
+        time.sleep(3)
+
+        # unsuscribe
+        self.memory.unsubscribeToEvent(
+            "SpeechRecognition/WordRecognized",
+            self.getName()
+        )
+
+
+    def talked_at(
+            self,
+            event_name,
+            word,
+            subscriber_identifier
+    ):
+        # capture whats being said
+
+        # detect specific commands
+        # match to known commands
+        for posture, posture_vocabulary in postures_vocabularies:
+            if word in posture_vocabulary:
+                print("{} recognized".format(word))
+                print("Going to execute {}".format(posture))
+                self.posture_manager.goToPosture(posture, 1)
+
+        # stop capturing what was being said
+
+        pass
 
     def wave(self):
         
-        # engage with the person that will be waved
-        self.awareness.engagePerson(self.waved_person_id)
-
-        # pause awareness
-        self.awareness.stopAwareness()
-
-        # greet, gets an animation future to be able to 
-        # animation_future = self.animation_player.runTag("hello")
-        self.text_to_speech.say("Hi, my name is chookie")
-
-        # wait for the future to end
-        # animation_future.wait(10000)
-
-        #resume awareness
-        self.awareness.startAwareness()
+        self.text_to_speech.say("bip bop")
         
 
     def on(self):
@@ -103,7 +141,6 @@ if __name__ == "__main__":
         print("error :(")
         sys.exit(1)
 
-    greetModule = GreetController(IP, PORT, session)
+    commandController = CommandController(IP, PORT, session)
 
-    greetModule.start_awareness()
-    
+    commandController.start_awareness()
