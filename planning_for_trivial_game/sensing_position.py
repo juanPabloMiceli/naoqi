9f8d463a3ff08efd51aec8641428fa79a9a6e6a5
Juan Pablo Miceli <micelijuanpablo@gmail.com>
2023-10-31 23:16:13 -0300

    Clean unused files

--- planning_for_trivial_game/sensing_position.py
+++
@@ -1,7 +0,0 @@
-class SensingPosition():
-    def __init__(self, memory):
-        self.memory = memory
-
-    def sense(self):
-        x, y = self.memory.get_player_position()
-        self.memory.event_queue.put(f"{x}{y}")
