65ac3a6d6d913005d39438df3187a50368398f2f
juanPabloMiceli <48705702+juanPabloMiceli@users.noreply.github.com>
2024-06-17 14:39:21 -0300

    Add text_to_speech (#7)

---
+++ workspace/cli/parsers/tts_parser.py
@@ -0,0 +1,7 @@
+def say(args, nao):
+    nao.say(args.text)
+
+def add_parser(subparser):
+    parser = subparser.add_parser('say', help='Make Nao say something')
+    parser.add_argument('text', type = str, help = 'What you want Nao to say')
+    parser.set_defaults(func=say)
